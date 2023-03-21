bce6ffb35dbff77669b2dc89b8178b3b31e11ce2
Ben Brostoff <ben.brostoff@gmail.com>
2023-03-21 10:52:55 -0400

    Okay, now onto certs

--- src/pages/api/passkit_stream.ts
+++ src/pages/api/passkit_stream.ts
@@ -19,6 +19,7 @@ const getStream = async () => {
   });
 }
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const stream = await getStream();
   res.status(200).json({ text: 'Hello' });
 }
