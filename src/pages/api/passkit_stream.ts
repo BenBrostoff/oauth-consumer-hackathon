f22138b0dde18eb8d30d459de8a1e74d8be44169
Ben Brostoff <ben.brostoff@gmail.com>
2023-03-21 10:48:28 -0400

    Generate a basic api endpoint

---
+++ src/pages/api/passkit_stream.ts
@@ -0,0 +1,24 @@
+import { PKPass } from "passkit-generator";
+
+const getStream = async () => {
+  const pass = await PKPass.from({
+    /**
+     * Note: .pass extension is enforced when reading a
+     * model from FS, even if not specified here below
+     */
+    model: "./passModels/myFirstModel.pass",
+    certificates: {
+      // wwdr,
+      // signerCert,
+      // signerKey,
+      // signerKeyPassphrase8
+    },
+  }, {
+    // keys to be added or overridden
+    serialNumber: "AAGH44625236dddaffbda"
+  });
+}
+
+export default function handler(req, res) {
+  res.status(200).json({ text: 'Hello' });
+}
